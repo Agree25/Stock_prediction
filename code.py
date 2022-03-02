ee03cb100b9ca6059f1a41487a4eaa3a01ddb690
Agree <kushagra.vdesk@gmail.com>
2022-03-02 20:42:46 +0530

    comments removed

--- code.py
+++ code.py
@@ -1,93 +1,73 @@
 
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 
-df=pd.read_csv(r'Data.csv', encoding = "ISO-8859-1")
+df = pd.read_csv(r'Data.csv', encoding="ISO-8859-1")
 
 print(df.head())
 
-#if label is 1 it means stock price will increase with respect to particular headlines,if 0 it means it will either constant or decrease
 
 train = df[df['Date'] < '20150101']
 test = df[df['Date'] > '20141231']
 
-data=train.iloc[:,2:27]
+data = train.iloc[:, 2:27]
 
 print(data.columns)
 
-# Removing punctuations
-#apart from a-z ,remove all by blank 
-data.replace("[^a-zA-Z]"," ", inplace=True)
+
+data.replace("[^a-zA-Z]", " ", inplace=True)
 
 print(data.head())
 
-# Renaming column names for ease of access
-list1= [i for i in range(25)]
-new_Index=[str(i) for i in list1]
+list1 = [i for i in range(25)]
+new_Index = [str(i) for i in list1]
 print(new_Index)
 
-data.columns= new_Index
+data.columns = new_Index
 
 
-
-### accessing data of 1st column
-print(data['0'])
-
-
-
-# Convertng headlines to lower case
-#why str.lower on 0,1,2---- bcz we can apply function on a column only
 for index in new_Index:
-    data[index]=data[index].str.lower()
+    data[index] = data[index].str.lower()
 print(data.head(1))
 
-##print entire data of 2nd row
-print(data.iloc[1,0:25])
 
-#create a list of all headlines for row 2,as 25 headlines in row 2
-#' '.join([str(x) for x in data.iloc[1,0:25]])
-
-## create a list headlines to insert all the headlines of all rows
-headlines=[]
-for row in range(0,len(data)):
-    headlines.append(' '.join([str(i) for i in data.iloc[row,0:25]]))
-
-#headlines[0:3]
-
+headlines = []
+for row in range(0, len(data)):
+    headlines.append(' '.join([str(i) for i in data.iloc[row, 0:25]]))
 
 
 """#### Implement Bag of Words"""
 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.ensemble import RandomForestClassifier
 
-## implement BAG OF WORDS
-cv=CountVectorizer(ngram_range=(2,2))
-traindata_x=cv.fit_transform(headlines)
+cv = CountVectorizer(ngram_range=(2, 2))
+traindata_x = cv.fit_transform(headlines)
 
-# implement RandomForest Classifier,almost takes 2 mins
-randomclassifier=RandomForestClassifier(n_estimators=200,criterion='entropy')
-randomclassifier.fit(traindata_x,train['Label'])
 
-## Predict for the Test Dataset
-test_transform= []
-for row in range(0,len(test)):
-    test_transform.append(' '.join(str(x) for x in test.iloc[row,2:27]))
+randomclassifier = RandomForestClassifier(
+    n_estimators=200, criterion='entropy')
+randomclassifier.fit(traindata_x, train['Label'])
+
+
+test_transform = []
+for row in range(0, len(test)):
+    test_transform.append(' '.join(str(x) for x in test.iloc[row, 2:27]))
 
 test_data = cv.transform(test_transform)
 
-randomclassifier.fit(traindata_x,train['Label'])
+randomclassifier.fit(traindata_x, train['Label'])
 
 predictions = randomclassifier.predict(test_data)
 
 
-
-
-
 """### plot_confusion_matrix"""
 
-from sklearn import metrics
-import numpy as np
-import matplotlib.pyplot as plt
+
 def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues, labels=["positive", "negative"]):
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
@@ -99,35 +79,32 @@ def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues, label
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
 
+
 plt.figure()
-cm=confusion_matrix(test['Label'],predictions)
+cm = metrics.confusion_matrix(test['Label'], predictions)
 print(cm)
-plot_confusion_matrix(cm)    
+plot_confusion_matrix(cm)
 plt.show()
 
-## Import library to check accuracy
-from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+# Import library to check accuracy
 
-score=accuracy_score(test['Label'],predictions)
+score = accuracy_score(test['Label'], predictions)
 print(score)
-report=classification_report(test['Label'],predictions)
+report = classification_report(test['Label'], predictions)
 print(report)
-
 
 
 """### Implement MultinomialNB"""
 
-from sklearn.naive_bayes import MultinomialNB
-nb=MultinomialNB()
-nb.fit(traindata_x,train['Label'])
+nb = MultinomialNB()
+nb.fit(traindata_x, train['Label'])
 
 predictions = nb.predict(test_data)
-matrix=confusion_matrix(test['Label'],predictions)
+matrix = confusion_matrix(test['Label'], predictions)
 print(matrix)
-score=accuracy_score(test['Label'],predictions)
+score = accuracy_score(test['Label'], predictions)
 print(score)
-report=classification_report(test['Label'],predictions)
+report = classification_report(test['Label'], predictions)
 print(report)
 
 plot_confusion_matrix(matrix)
-
